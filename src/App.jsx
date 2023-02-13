b111725b9c650d7111f4a67571f10b3272f0dd8a
weiyuan12 <weiyuan.tan0@gmail.com>
2023-02-14 00:03:15 +0800

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EntranceScreen from './components/EntranceScreen'
+export default function App(){
+    return(
+        <div>
+            <EntranceScreen/>
+        </div>
+    )
+}
---
+++ src/components/EntranceScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function EntranceScreen(){
+    return(
+        <div>
+            <h1>
+                Wanderful
+            </h1>
+        </div>
+    )
+}
