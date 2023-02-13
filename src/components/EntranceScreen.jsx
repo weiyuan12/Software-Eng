e311a2fbf171d35da34bb814da11a0e5aa7ec35b
weiyuan12 <weiyuan.tan0@gmail.com>
2023-02-14 02:31:10 +0800

    Created navbar for entrance

--- src/components/EntranceScreen.jsx
+++ src/components/EntranceScreen.jsx
@@ -1,11 +1,34 @@
 import React from "react"
-
+import logo from "../assets/logo.png"
+import {CiLogin, CiEdit, CiSettings} from "react-icons/ci"
 export default function EntranceScreen(){
     return(
         <div>
-            <h1>
-                Wanderful
-            </h1>
+            <nav className="entrance-nav">            
+                <img src = {logo} className= "logo"/>
+                <div className="options">
+                    <button className="signup">
+                        <CiEdit style={{fontSize : "30px", marginRight:"10px"}}/>
+                        <h3>Signup</h3>  
+                    </button>
+                    <button className="login">
+                        <CiLogin style={{fontSize : "30px", marginRight:"10px"}}/>
+                        <h3>Login</h3>  
+                    </button>
+                    <button>
+                        <CiSettings style={{fontSize : "30px"}}/>
+                    </button>
+                </div>
+                
+                      
+            </nav>
+            <main>
+                <div className="body-text">
+                    <h1 className="text-1">Wander around safely and efficiently</h1>
+                    <h2 className="text-2">Navigate with ease using Wanderful as we aim to support the lifestyle that you want!</h2>
+                </div>
+                
+            </main>
         </div>
     )
 }
