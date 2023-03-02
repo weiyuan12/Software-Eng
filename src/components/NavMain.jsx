eae44924937e362f833d415df22e72f8b52656bf
Hexumicx <jonathanjwx@gmail.com>
2023-03-02 21:19:11 +0800

    Navbar and settings

--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import "../styles/Nav.css"
 import {Link} from "react-router-dom"
 import { SelectionContext, UserContext } from './Usercontext';
+import Settings from './Settings';
 
 export default function NavMain(props) {
     const {user} = useContext(UserContext);
@@ -11,22 +12,21 @@ export default function NavMain(props) {
         if(a===selection){
             return (
                 <li key={a} className='nav-tab-selected' onClick={()=>{setSelection(a)}}>
-                    <Link to="/main">{a}</Link>
+                    <Link style={{height:"100%", width:"100%",display:"flex",alignItems:"center"}} to="/main">{a}</Link>
                 </li>
             )
         }
         return (
             <li key={a} className='nav-tab' onClick={()=>{setSelection(a)}}>
-                <Link to="/main">{a}</Link>
+                <Link style={{height:"100%", width:"100%",display:"flex",alignItems:"center"}} to="/main">{a}</Link>
             </li>
         );
     })
-    console.log(props);
     return (
         <nav className='header'> 
             <div style={{height:'100%',display:"flex"}}>
                 <div className='Logobar'>
-                    <Link to="/">
+                    <Link onClick={()=>setSelection("home")} to="/">
                         <img className='logo' src='assets/Logomain.png'/>
                     </Link>
                 </div>
@@ -35,17 +35,17 @@ export default function NavMain(props) {
                 </ul>
             </div>
             <div className='Accessbar'>
-                <button className='invis'>   
-                    <Link to="/">
-                        <img className='profile' src='assets/IconProfile.png'/>
-                        Welcome {user.name}
-                    </Link>
-                </button>
-                <button className='invis'>
-                    <Link to="/settings">
-                        <img className='setting' src='assets/IconSetting.png'/>
-                    </Link>
-                </button>
+                <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
+                    <img className='profile' src='assets/IconProfile.png'/>
+                    <p style={{margin:"0px"}}>Welcome {user.name}</p>
+                </div>
+                <div className={selection==="Settings" ? 'nav-tab-selected' : 'nav-tab'} style={{width:"40px",marginLeft:"10px",marginRight:"10px"}}>
+                    <button className='invis' onClick={()=>setSelection("Settings")}>
+                        <Link to="/settings">
+                            <img className='setting' src='assets/IconSetting.png'/>
+                        </Link>
+                    </button>
+                </div>
             </div>
             
         </nav>
