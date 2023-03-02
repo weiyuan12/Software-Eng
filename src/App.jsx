96d8c96fd1db15ac752e346af0ce7cbc94b7d11d
Hexumicx <jonathanjwx@gmail.com>
2023-03-02 17:30:09 +0800

    Moved Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,34 @@ import Dashboard from './components/Dashboard';
 import Signup from './components/SignupScreen';
 import Main from './components/Main';
 import DynamicMap from './components/Map';
+import Navbar from './components/NavGuest';
+import NavMain from './components/NavMain';
 import "./styles/DefaultStyles.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { UserContext } from './components/Usercontext';
+import { UserContext ,SelectionContext } from './components/Usercontext';
 import Settings from './components/Settings';
 
 export default function App(){
     const [user, setUser] = useState({name:"John",id:"404"});
+    const [selection, setSelection] = useState("");
     return(
-        <BrowserRouter>
-            <UserContext.Provider value={{user, setUser}}>
-                <Routes>
-                    <Route path="/" element={<EntranceScreen/>}/>
-                    <Route path="/dashboard" element={<Dashboard/>}/>
-                    <Route path="/login" element={<LoginScreen/>}/>
-                    <Route path="/signup" element={<Signup/>}/>
-                    <Route path="/main" element={<Main/>}/>
-                    <Route path="/settings" element={<Settings/>}/>
-                </Routes>
-            </UserContext.Provider>
-        </BrowserRouter>
+        <div>
+            <BrowserRouter>
+                <UserContext.Provider value={{user, setUser}}>
+                <SelectionContext.Provider value={{selection, setSelection}}>
+                    {user.id ? <NavMain/> : <Navbar/>}
+                </SelectionContext.Provider>
+                    <Routes>
+                        <Route path="/" element={<EntranceScreen/>}/>
+                        <Route path="/dashboard" element={<Dashboard/>}/>
+                        <Route path="/login" element={<LoginScreen/>}/>
+                        <Route path="/signup" element={<Signup/>}/>
+                        <Route path="/main" element={<Main/>}/>
+                        <Route path="/settings" element={<Settings/>}/>
+                    </Routes>
+                </UserContext.Provider>
+            </BrowserRouter>
+        </div>
+        
     )
 }
